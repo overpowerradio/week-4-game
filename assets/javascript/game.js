e67cbdd2678c61a90d469844da2ed03d456bd193
Dj Will Power <Dj Will Power>
2017-05-24 12:20:09 -0400

    cleaned up the Psuedo Code in my js file for what is visible

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,63 @@
+// Creating a variable for all 4 characters to choose from
+var charList = []; 
 
-var charList = []; // all images within the .character class
-var enemyLineUp = [];
- //whatever image we clicked on
+//Creating a variable for the character the user chooses/clicks on
 var myCharacter;
-var isPicked = false;
 
+//Creating a variable to store the remaining 3 characters as enemies
+var enemyLineUp = [];
+
+//Creating a variable/boolean (true/false) I can use to limit/lock in a function (below). Once I clicks a character it becomes "My Character" and is moved to the appropriate <div>. A boolean (true/false) variable will help me classify the user's character choice and write conditional statements (if, else if, else) so that if another character is clicked afterwards, it will not become "My Characacter". 
+
+var isPicked = false; //means nothing has been picked
+
+
+//(dot each) .each  is a simple to way to write a loop using jQuery. I can still use "i" as a variable to loop through arrays and strings by their index. 
 $(".characters").each(function (i) {
-//All the images in the .character class should be put into the charList variable.
-charList[i] = $(this)
-//charList = (this);
-	// console.log(i);
-	// console.log($(this).attr("src"));
+
+	charList[i] = $(this)
+		//This is assigning all of the image files in the .characters class to my (charList) variable.
+
+		// console.log(i);
+		// console.log($(this).attr("src"));
 })
 
 console.log(charList);
 
-//When a character is clicked
-//That character should become "myCharacter"
-//And move to the "#yourCharcter" div
+//Here is where I write conditional statements, so when I click on a character in (charList), the character will become "myCharacter" and move to <div id="yourCharacter"> on the html page and simultaneiouly defining the enemyLineUp and putting the enemy images in their appropriate <div>
+
 $(".characters").on("click", function() {
+	
+
 	if (isPicked === false){
+		// Only if isPicked = false (meaning, I haven't chosen a character), will the system allow the next step.. 
+
 		myCharacter = $(this);
+
+		//...to make the character that I click on become (myCharacter).
+		 
+
 		isPicked = true;
+
+		// Now that I've picked a character, I can state [isPicked = true] and write 'for loops', conditionals, and functions that only applies once a character has been selected.
+
 		for (var i = 0; i < charList.length; i++) {
 
+		// If an index in the array of charList is not equal to what I clicked on, I want those arrays to be pushed into the variable I created for my (enemyLineUp).  ** I had to ad id tags on each image, because without it jQuery wasn't able compare the images and determine if they were different or equal, so...
+
 			if ( charList[i].attr("id") !== $(this).attr("id") ) {
-				//this will only run IF it is NOT myCharacter
+			
 				enemyLineUp.push(charList[i])
 
 			}
 		}
-		console.log(enemyLineUp);
-		console.log(myCharacter);
-		$("#Enemies").append(enemyLineUp);
-		$("#yourCharacter").append(myCharacter);
+		//console.log(enemyLineUp); to check that it works
+		//console.log(myCharacter); to check that it works
 
+		$("#Enemies").append(enemyLineUp); //Once the click is executed, this appends the #Enemies <div> and puts the enemyLineUp images there.
+		$("#yourCharacter").append(myCharacter); //Once the click is executed, this appends the #yourCharacter <div> and puts the enemyLineUp images there.
+
+		
 	//else {
 		//code to run on all subsequent clicks
 	//}
